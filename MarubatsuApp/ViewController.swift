0b609e9247cff6663e8638cd0237cac5b623084e
Y-Ito-55 <fortunekiss4u@icloud.com>
2023-09-05 15:58:43 +0900

    作成途中

--- MarubatsuApp/ViewController.swift
+++ MarubatsuApp/ViewController.swift
@@ -1,78 +1,91 @@
 //
 //  ViewController.swift
-//  MarubatsuApp
 //
-//  Created by Yumi Ito on 2023/09/02.
+//
+//  Created by Ngos on
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-    
     @IBOutlet weak var questionLabel: UILabel!
     
-    @IBOutlet weak var Segment: UISegmentedControl!   //宿題で追加
     
-    // 表示中の問題番号を格納
+    
+    // Put Question number
     var currentQuestionNum: Int = 0
+    
     // 問題
-    let questions: [[String: Any]] = [
-        [
-            "question": "iPhoneアプリを開発する統合環境はZcodeである",
-            "answer": false
-        ],
-        [
-            "question": "Xcode画面の右側にはユーティリティーズがある",
-            "answer": true
-        ],
-        [
-            "question": "UILabelは文字列を表示する際に利用する",
-            "answer": true
-        ]
+    var questions: [[String: Any]] = [
+//        [
+//            "question": "iPhoneアプリを開発する統合環境はZcodeである",
+//            "answer": false
+//        ],
+//        [
+//            "question": "Xcode画面の右側にはユーティリティーズがある",
+//            "answer": true
+//        ],
+//        [
+//            "question": "UILabelは文字列を表示する際に利用する",
+//            "answer": true
+//        ]
     ]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        showQuestion()
-        segmented.addTarget(self, action: #selector(segmentChanged), for: .valueChanged)  //宿題で追加
-        
+        showQestion()
+        // Display question after loading
     }
     
-    //問題を表示する関数
-    func showQuestion(){
+    
+    // Display question
+    func showQestion(){
+        if currentQuestionNum >= questions.count {
+            questionLabel.text = "問題がありません"
+            return
+        }
+        
         let question = questions[currentQuestionNum]
         
         if let que = question["question"] as? String {
             questionLabel.text = que
+        }
     }
     
-    //回答チェックの関数、正解なら次の問題を表示
+    // Check answer is corrent proceed next question
     func checkAnswer(yourAnswer: Bool){
+        // 現在の問題番号がquestionsの範囲外である場合、アラートを表示して関数を終了
+        if currentQuestionNum >= questions.count {
+            showAlert(message: "問題がありません")
+            return
+        }
         let question = questions[currentQuestionNum]
         
-        if let ans = question["answer"]as? Bool {
-            if yourAnswer == ans {     //正解したときの処理
-                currentQuestionNum += 1       //カレントクエスチョンなむに1を足して次の問題に進む
-                showAlert(message: "正解！")
-            } else {     //不正解のときの処理
-                showAlert(message: "不正解…")
+        if let ans = question["answer"] as? Bool{
+            if yourAnswer == ans {
+                // When correct answer
+                // Plus 1 to currentQuestionNum and next question
+                currentQuestionNum += 1
+                showAlert(message: "正解!")
+            } else {
+                // When incorrect
+                showAlert(message: "不正解!")
             }
             
-        } else {        //答えがない時
+        } else { // When not answer
             print("答えが入っていません")
             return
         }
-        
-        //カレントクエスチョンなむの値が問題数以上なら最初の問題へ
+        // if currentQuestionNum more tha qty of Question, back to initial question
         if currentQuestionNum >= questions.count {
             currentQuestionNum = 0
         }
         
-        showQuestion()     //正解→次の問題を表示、不正解→同じ問題を表示
         
+        showQestion() //Correct -> next Question, Incorrect -> Same question.
     }
     
-    // アラート(今回の場合、正解不正解の文字が出る小さい窓がポップする)を表示する関数
+    // アラートを表示する関数
     func showAlert(message: String) {
         let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
         let close = UIAlertAction(title: "閉じる", style: .cancel, handler: nil)
@@ -87,12 +100,21 @@ class ViewController: UIViewController {
     @IBAction func tappedYesButton(_ sender: Any) {
         checkAnswer(yourAnswer: true)
     }
-
-        
-}
-
-    // 選択された時に実行させたい処理 宿題で追加
-    @objc  func segmentChanged() {
-        let selectedIndex = segmented.selectedSegmentIndex
-        print(selectedIndex)
+    
+    // 問題作成画面に遷移するボタンのアクション
+    @IBAction func createQuestionButtonTapped(_ sender: Any) {
+        let createQuestionVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "CreateQuestionViewController") as! CreateQuestionViewController
+        createQuestionVC.questions = self.questions
+        createQuestionVC.delegate = self // ViewControllerをデリゲートとして設定
+        present(createQuestionVC, animated: true, completion: nil)
     }
+
+}
+// CreateQuestionViewControllerからのデータを受け取るための拡張
+extension ViewController: CreateQuestionDelegate {
+    func didUpdateQuestions(updatedQuestions: [[String : Any]]) {
+        self.questions = updatedQuestions
+        currentQuestionNum = 0
+        showQestion()
+    }
+}
